cbb87e96155755117614c0ce51298d237ede191d
migranthelp <migrantes.help@gmail.com>
2025-09-16 12:24:38 +0100

    added pages

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const App = () => {
+const CreatePage = () => {
   return (
     <div>
-      App
+      
     </div>
   )
 }
 
-export default App
+export default CreatePage
